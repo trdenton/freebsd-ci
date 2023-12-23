074cf363ed721268e027ea78c0c1fdbe784f6e7b
troy denton <trdenton@gmail.com>
2023-12-22 21:12:00 -0500

    testing

--- scripts/test/subr/run-ltp-posix.sh
+++ scripts/test/subr/run-ltp-posix.sh
@@ -7,9 +7,10 @@ echo "--------------------------------------------------------------"
 
 set +e
 #fetch https://github.com/linux-test-project/ltp/releases/download/20230929/ltp-full-20230929.tar.xz
-tar xf ltp-full-20230929.tar.xz
+pwd
+ls
+tar xf /meta/ltp-full-20230929.tar.xz
 
-
-cd ./ltp-full-20230929
+cd ltp-full-20230929
 python3 ../run.py test ltp_out.json
 python3 ../format_junit.py ltp_out.json ltp_junit.xml
