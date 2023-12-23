f37b1d2e74dea005960a37165022081339515458
troy denton <trdenton@gmail.com>
2023-12-22 22:06:07 -0500

    pkg inst, network if

--- scripts/test/subr/run-ltp-posix.sh
+++ scripts/test/subr/run-ltp-posix.sh
@@ -11,5 +11,7 @@ ls /meta
 tar xf /meta/ltp-full-20230929.tar.xz
 
 cd ltp-full-20230929
+pkg inst -y gmake
+pkg inst -y py32-junit-xml
 python3 /meta/run.py test ltp_out.json
 python3 /meta/format_junit.py ltp_out.json ltp_junit.xml
